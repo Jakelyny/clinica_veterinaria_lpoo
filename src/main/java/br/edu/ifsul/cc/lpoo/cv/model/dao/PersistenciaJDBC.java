fe131b66ba829afa5caf2ba6a89a552f3c0e8fe8
Jakelyny <jakelynysousa.a@gmail.com>
2022-01-26 10:17:08 -0300

    Alteração em persistência JDBC,  persistência xml como none e Inserção de médico em TestPersistenciaJDBC

--- src/main/java/br/edu/ifsul/cc/lpoo/cv/model/dao/PersistenciaJDBC.java
+++ src/main/java/br/edu/ifsul/cc/lpoo/cv/model/dao/PersistenciaJDBC.java
@@ -7,8 +7,10 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Date;
+import java.sql.Date;
+import java.util.List;
 
 /**
  *
@@ -61,10 +63,12 @@ public class PersistenciaJDBC implements InterfacePersistencia {
         
         if(c == Pessoa.class){
             
-            PreparedStatement ps = this.con.prepareStatement("select cpf, rg, nome, senha, numero_celular, email, data_cadastro, data_nascimento, cep, endereco, complemento from tb_pessoa where cpf = ? ");
-            ps.setInt(1, Integer.parseInt(id.toString()));
+            PreparedStatement ps = this.con.prepareStatement("select cpf, rg, nome, senha, numero_celular, email,"
+                    + " data_cadastro, data_nascimento, cep, endereco, complemento from tb_pessoa where cpf = ? ");
             
+            ps.setInt(1, Integer.parseInt(id.toString()));
             ResultSet rs = ps.executeQuery();
+            
             if(rs.next()){
             
                 Pessoa p = new Pessoa();
@@ -85,7 +89,6 @@ public class PersistenciaJDBC implements InterfacePersistencia {
                 p.setComplemento(rs.getString("complemento"));
                 
                 ps.close();
-                
                 return p;                
             }
             
@@ -94,7 +97,6 @@ public class PersistenciaJDBC implements InterfacePersistencia {
             PreparedStatement ps = this.con.prepareStatement("select cpf, numero_crmv from tb_medico where cpf = ?");
             
             ps.setInt(1, Integer.parseInt(id.toString()));
-            
             ResultSet rs = ps.executeQuery();
 
             if(rs.next()){
@@ -107,7 +109,6 @@ public class PersistenciaJDBC implements InterfacePersistencia {
                 m.setData_cadastro(dm);
                                
                 ps.close();
-                
                 return m;                
             }            
         }        
@@ -123,10 +124,10 @@ public class PersistenciaJDBC implements InterfacePersistencia {
 
             Pessoa p = (Pessoa) o;
 
-            if (p.getCpf() == null) {
+            if (p.getData_cadastro() == null) {
                 PreparedStatement ps = this.con.prepareStatement("insert into tb_pessoa "
-                        + "(cpf, , rg, nome, senha, numero_celular, email, data_cadastro, data_nascimento, cep, endereco, "
-                        + "complemento) values " + "(now(),?,?,?,?,?,?,?,?,?,?,?)");
+                        + "(data_cadastro, cpf, rg, nome, senha, numero_celular, email, data_nascimento, cep, endereco, "
+                        + "complemento) values " + "(now(),?,?,?,?,?,?,?,?,?,?)");
 
                 ps.setString(1, p.getCpf());
                 ps.setString(2, p.getRg());
@@ -134,15 +135,12 @@ public class PersistenciaJDBC implements InterfacePersistencia {
                 ps.setString(4, p.getSenha());
                 ps.setString(5, p.getNumero_celular());
                 ps.setString(6, p.getEmail());
-                Date dc = new Date(System.currentTimeMillis());
-                dc.setTime(p.getData_cadastro().getTimeInMillis());
-                ps.setDate(7, (java.sql.Date) dc);
-                Date dn = new Date(System.currentTimeMillis());
-                dn.setTime(p.getData_nascimento().getTimeInMillis());
-                ps.setDate(8, (java.sql.Date) dn);
-                ps.setString(9, p.getCep());
-                ps.setString(10, p.getEndereco());
-                ps.setString(11, p.getComplemento());
+                Date dtU = new Date(System.currentTimeMillis());
+                dtU.setTime(p.getData_nascimento().getTimeInMillis());
+                ps.setDate(7, dtU);
+                ps.setString(8, p.getCep());
+                ps.setString(9, p.getEndereco());
+                ps.setString(10, p.getComplemento());
 
                 System.out.println("Insert em Pessoa");               
                 ps.executeUpdate();
@@ -150,24 +148,22 @@ public class PersistenciaJDBC implements InterfacePersistencia {
                 
                 PreparedStatement ps = this.con.prepareStatement("update tb_pessoa set "
                         + "rg = ?, " + "nome = ?, " + "senha = ?, " + "numero_celular = ?, " + "email = ? "
-                        + "data_cadastro = ? " + "data_nascimento = ? " + "cep = ? " + "endereco = ? " + "complemento = ? "
+                        + "data_nascimento = ? " + "cep = ? " + "endereco = ? " + "complemento = ? "
                         + "where cpf = ?");
                 
-                ps.setString(1, p.getCpf());
-                ps.setString(2, p.getRg());
-                ps.setString(3, p.getNome());
-                ps.setString(4, p.getSenha());
-                ps.setString(5, p.getNumero_celular());
-                ps.setString(6, p.getEmail());
-                Date dc = new Date(System.currentTimeMillis());
-                dc.setTime(p.getData_cadastro().getTimeInMillis());
-                ps.setDate(7, (java.sql.Date) dc);
+                ps.setString(1, p.getRg());
+                ps.setString(2, p.getNome());
+                ps.setString(3, p.getSenha());
+                ps.setString(4, p.getNumero_celular());
+                ps.setString(5, p.getEmail());
                 Date dn = new Date(System.currentTimeMillis());
                 dn.setTime(p.getData_nascimento().getTimeInMillis());
-                ps.setDate(8, (java.sql.Date) dn);
-                ps.setString(9, p.getCep());
-                ps.setString(10, p.getEndereco());
-                ps.setString(11, p.getComplemento());
+                ps.setDate(6, (java.sql.Date) dn);
+                ps.setString(7, p.getCep());
+                ps.setString(8, p.getEndereco());
+                ps.setString(9, p.getComplemento());
+                ps.setString(10, p.getCpf());
+
                 
                 System.out.println("Update em Pessoa");               
                 ps.execute();
@@ -178,7 +174,7 @@ public class PersistenciaJDBC implements InterfacePersistencia {
 
              if (m.getData_cadastro_medico() == null) {
                 PreparedStatement ps = this.con.prepareStatement("insert into tb_medico "
-                        + "(cpf, numero_crmv, data_cadastro_medico) values " + "(now(),?, ?, ?)");
+                        + "(data_cadastro_medico, cpf, numero_crmv) values " + "(now(),?,?)");
                 ps.setString(1, m.getCpf());
                 ps.setString(2, m.getNumero_crmv());
                 System.out.println("Insert Medico");
@@ -191,8 +187,8 @@ public class PersistenciaJDBC implements InterfacePersistencia {
                 ps.setString(1, m.getCpf());
                 ps.setString(2, m.getNumero_crmv());
                 Date dc = null;
-                dc.setTime(m.getData_cadastro().getTimeInMillis());
-                ps.setDate(3, (java.sql.Date) dc);
+                dc.setTime(m.getData_cadastro_medico().getTimeInMillis());
+                ps.setDate(3, dc);
                 System.out.println("Update Medico");
 
                 ps.execute();
@@ -219,5 +215,64 @@ public class PersistenciaJDBC implements InterfacePersistencia {
             ps.setString(1, m.getCpf());            
             ps.execute(); 
         } 
+    }
+    
+    public List<Pessoa> listPessoas() throws Exception {
+        
+        List<Pessoa> lista = null;
+                        
+        PreparedStatement ps = this.con.prepareStatement("select cpf, rg, nome, senha, numero_celular, email,"
+                    + " data_cadastro, data_nascimento, cep, endereco, complemento from tb_pessoa");
+        
+        ResultSet rs = ps.executeQuery();//executa a query        
+      
+        lista = new ArrayList();
+        while(rs.next()){
+            
+            Pessoa p = new Pessoa();
+            p.setCpf(rs.getString("cpf"));
+            p.setRg(rs.getString("rg"));
+            p.setNome(rs.getString("nome"));
+            p.setSenha(rs.getString("senha"));
+            p.setNumero_celular(rs.getString("numero_celular"));
+            p.setEmail(rs.getString("email"));
+            Calendar dc = null;
+            dc.setTimeInMillis(rs.getDate("data_cadastro").getTime());
+            p.setData_cadastro(dc);
+            Calendar dn = null;
+            dn.setTimeInMillis(rs.getDate("data_nascimento").getTime());
+            p.setData_nascimento(dn);
+            p.setCep(rs.getString("cep"));
+            p.setEndereco(rs.getString("endereco"));
+            p.setComplemento(rs.getString("complemento"));
+            
+            lista.add(p);//adiciona na lista o objetivo que contem as informações de um determinada linha do ResultSet.
+            
+        }                
+        return lista;
+    }
+    
+    public List<Medico> listMedicos() throws Exception {
+        
+        List<Medico> lista = null;
+                        
+        PreparedStatement ps = this.con.prepareStatement("select cpf, numero_crmv from tb_medico");
+        
+        ResultSet rs = ps.executeQuery();//executa a query        
+      
+        lista = new ArrayList();
+        while(rs.next()){
+            
+            Medico m = new Medico();
+            m.setCpf(rs.getString("cpf"));
+            m.setNumero_crmv(rs.getString("numero_crmv"));
+            Calendar dc = null;
+            dc.setTimeInMillis(rs.getDate("data_cadastro_medico").getTime());
+            m.setData_cadastro(dc);
+            
+            lista.add(m);//adiciona na lista o objetivo que contem as informações de um determinada linha do ResultSet.
+            
+        }                
+        return lista;
     }
 }
--- src/test/java/br/edu/ifsul/cc/lpoo/cv/test/TestPersistenciaJDBC.java
+++ src/test/java/br/edu/ifsul/cc/lpoo/cv/test/TestPersistenciaJDBC.java
@@ -1,9 +1,77 @@
 package br.edu.ifsul.cc.lpoo.cv.test;
 
+import br.edu.ifsul.cc.lpoo.cv.model.Medico;
+import br.edu.ifsul.cc.lpoo.cv.model.dao.PersistenciaJDBC;
+import java.util.Calendar;
+import java.util.List;
+import org.junit.Test;
+
 /**
  *
  * @author Jakelyny Sousa
  */
 public class TestPersistenciaJDBC {
     
+    @Test
+    public void testConexao() throws Exception {
+
+        PersistenciaJDBC persistencia = new PersistenciaJDBC();
+        if(persistencia.conexaoAberta()){
+            System.out.println("abriu a conexao com o BD via JDBC");
+
+            persistencia.fecharConexao();
+
+        }else{
+            System.out.println("Nao abriu a conexao com o BD via JDBC");
+        }
+
+    }
+
+    @Test
+    public void testListPersistenciaMedico() throws Exception {
+        
+        PersistenciaJDBC persistencia = new PersistenciaJDBC();
+        if(persistencia.conexaoAberta()){
+            
+            List<Medico> lista = persistencia.listMedicos();
+            
+            if(!lista.isEmpty()){
+            
+                for(Medico m : lista){
+                    System.out.println("\n-- Médico -- \n");
+                    System.out.println("Cpf: "+m.getCpf()+ " \nNumero CRMV: "+m.getNumero_crmv() + " \nData de Cadastro: "+m.getData_cadastro_medico());
+                    
+                    persistencia.remover(m);
+                }
+            }else{
+                
+                System.out.println("NÃO encontrou o médico");
+                
+                Medico m = new Medico();
+                m.setCpf("091.536.910-91");
+                m.setNumero_crmv("1111");    
+                Calendar data_cadastro_m = Calendar.getInstance();
+                data_cadastro_m.set(2022,02,04);
+                m.setData_cadastro_medico(data_cadastro_m);
+                
+                persistencia.persist(m); //insert na tabela.                
+                System.out.println("Cadastrou o medico do CPF "+m.getCpf());
+
+                m = new Medico();//reset com a nova instancia que é gerada aqui.
+                m.setCpf("748.995.640-40");
+                m.setNumero_crmv("2222");    
+                Calendar data_cadastro_me = Calendar.getInstance();
+                data_cadastro_me.set(2022,05,06);
+                m.setData_cadastro_medico(data_cadastro_me);
+                
+                persistencia.persist(m); //insert na tabela.
+                System.out.println("Cadastrou o medico do CPF "+m.getCpf());
+                
+            }
+            
+            persistencia.fecharConexao();
+        }else{
+            System.out.println("Nao abriu a conexao com o BD via JDBC");
+        }
+    }
 }
