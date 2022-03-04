7c1fa7a62e61221926b296b9fce0c960de0b0ae8
Jakelyny <jakelynysousa.a@gmail.com>
2022-03-04 09:12:10 -0300

    Update TestPersistenciaJPA.java

--- src/test/java/br/edu/ifsul/cc/lpoo/cv/test/TestPersistenciaJPA.java
+++ src/test/java/br/edu/ifsul/cc/lpoo/cv/test/TestPersistenciaJPA.java
@@ -1,4 +1,6 @@
 package br.edu.ifsul.cc.lpoo.cv.test;
+import br.edu.ifsul.cc.lpoo.cv.model.Medico;
+import br.edu.ifsul.cc.lpoo.cv.model.Pessoa;
 import br.edu.ifsul.cc.lpoo.cv.model.dao.PersistenciaJPA;
 import java.util.List;
 import org.junit.Test;
@@ -21,5 +23,34 @@ public class TestPersistenciaJPA {
         }else{
             System.out.println("Nao abriu a conexao com o BD via JPA");
         }
+    }
+    
+     @Test
+    public void testGeracaoPessoaLogin() throws Exception {
+
+        PersistenciaJPA persistencia = new PersistenciaJPA();
+        if(persistencia.conexaoAberta()){
+            System.out.println("abriu a conexao com o BD via JPA");
+
+            Pessoa p = persistencia.doLogin("teste@", "1234");
+
+            if(p == null){
+                p = new Pessoa();
+                p.setNome("teste@");
+                p.setSenha("1234");
+
+                persistencia.persist(p);
+                System.out.println("Cadastrou nova pessoa!");
+            }else{
+                System.out.println("Encontrou pessoa cadastrada!");
+            }
+
+
+            persistencia.fecharConexao();
+
+        }else{
+            System.out.println("Nao abriu a conexao com o BD via JPA");
+        }
+
     }
 }
