2267b4c81f0ed9a00449542d3bfa3861376eae97
Jakelyny <jakelynysousa.a@gmail.com>
2022-03-10 11:09:48 -0300

    Update JPanelAutenticacao.java

--- src/main/java/br/edu/ifsul/cc/lpoo/cv/gui/autenticacao/JPanelAutenticacao.java
+++ src/main/java/br/edu/ifsul/cc/lpoo/cv/gui/autenticacao/JPanelAutenticacao.java
@@ -22,9 +22,9 @@ public class JPanelAutenticacao extends JPanel implements ActionListener {
     private GridBagLayout gridLayout;
     private GridBagConstraints posicionador;
 
-    private JLabel  lblNome;
+    private JLabel lblCpf;
     private JLabel lblSenha;
-    private JTextField txfNome;
+    private JTextField txfCpf;
     private JPasswordField psfSenha;
     private JButton btnLogar;
 
@@ -40,21 +40,21 @@ public class JPanelAutenticacao extends JPanel implements ActionListener {
         gridLayout = new GridBagLayout();//inicializando o gerenciador de layout
         this.setLayout(gridLayout);//definie o gerenciador para este painel.
 
-        lblNome = new JLabel("Nome:");
-        lblNome.setFocusable(true);    //acessibilidade    
-        lblNome.setToolTipText("lblNome"); //acessibilidade
+        lblCpf = new JLabel("CPF:");
+        lblCpf.setFocusable(true);    //acessibilidade    
+        lblCpf.setToolTipText("lblCpf"); //acessibilidade
         posicionador = new GridBagConstraints();
         posicionador.gridy = 0;//policao da linha (vertical)
         posicionador.gridx = 0;// posição da coluna (horizontal)
-        this.add(lblNome, posicionador);//o add adiciona o rotulo no painel
+        this.add(lblCpf, posicionador);//o add adiciona o rotulo no painel
 
-        txfNome = new JTextField(10);
-        txfNome.setFocusable(true);    //acessibilidade    
-        txfNome.setToolTipText("txfNome"); //acessibilidade
+        txfCpf = new JTextField(10);
+        txfCpf.setFocusable(true);    //acessibilidade    
+        txfCpf.setToolTipText("txfNome"); //acessibilidade
         posicionador = new GridBagConstraints();
         posicionador.gridy = 0;//policao da linha (vertical)
         posicionador.gridx = 1;// posição da coluna (horizontal)
-        this.add(txfNome, posicionador);//o add adiciona o rotulo no painel        
+        this.add(txfCpf, posicionador);//o add adiciona o rotulo no painel        
 
         lblSenha = new JLabel("Senha:");
         lblSenha.setFocusable(true);    //acessibilidade    
@@ -93,12 +93,12 @@ public class JPanelAutenticacao extends JPanel implements ActionListener {
         if(e.getActionCommand().equals(btnLogar.getActionCommand())){
 
             //validacao do formulario.
-            if(txfNome.getText().trim().length() > 4 && new String(psfSenha.getPassword()).trim().length() > 3 ){
+            if(txfCpf.getText().trim().length() > 4 && new String(psfSenha.getPassword()).trim().length() > 3 ){
 
-                controle.autenticar(txfNome.getText().trim(), new String(psfSenha.getPassword()).trim());
+                controle.autenticar(txfCpf.getText().trim(), new String(psfSenha.getPassword()).trim());
 
             } 
-            else if(txfNome.getText().trim().length() == 0){
+            else if(txfCpf.getText().trim().length() == 0){
                 JOptionPane.showMessageDialog(this, "Necessário inserir o nome!");
             }
             else if(new String(psfSenha.getPassword()).trim().length() == 0){
