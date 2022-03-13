a220930f9748122366b5cfc91ccefb59d30de5de
Jakelyny <jakelynysousa.a@gmail.com>
2022-03-13 09:47:36 -0300

    Update TestPersistenciaJDBC.java

--- src/test/java/br/edu/ifsul/cc/lpoo/cv/test/TestPersistenciaJDBC.java
+++ src/test/java/br/edu/ifsul/cc/lpoo/cv/test/TestPersistenciaJDBC.java
@@ -118,6 +118,8 @@ public class TestPersistenciaJDBC {
         }
     }
     
+    
+    
     @Test //Insert dos dados das tabelas funcionário e médico
     public void testInsertFuncionario() throws Exception {
         
